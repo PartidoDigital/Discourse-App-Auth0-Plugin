af1c05a8ecab993aad507305373f128e651d91be
Leo Giovanetti <hello@leog.me>
2016-10-30 02:44:37 -0300

    Moviendo opciones de Lock para otro contexto

--- assets/javascripts/auth0.js
+++ assets/javascripts/auth0.js
@@ -16,44 +16,6 @@
   }
 
   var lock;
-  
-  var lock_options = {
-	language: "es",
-	auth: {
-          	responseType: 'code',
-          	redirectUrl:  Discourse.SiteSettings.auth0_callback_url
-	},
-	theme: {
-		logo: "https://recursos.partidodigital.org.uy/assets/img/logo_original.svg",
-		primaryColor: "#F37021"
-	},
-	allowForgotPassword: true,
-	allowLogin: true,
-	loginAfterSignUp: false,
-	mustAcceptTerms: true,
-	languageDictionary: {
-		signUpTerms: "Acepto los <a href='https://partidodigital.com/documentos/terminos-de-uso' target='_new'>términos de uso</a> y <a href='https://partidodigital.com/documentos/privacidad-de-datos' target='_new'>privacidad de datos</a>.",
-		title: "Acceso",
-		success: {
-			signUp: "Registro completado exitosamente. Chequea tu correo para verificar tu dirección y seguir con los próximos pasos."
-		}
-	},
-	additionalSignUpFields: [{
-		name: "credencial",
-		placeholder: "ingrese su credencial",
-		icon: "https://recursos.partidodigital.org.uy/assets/img/credencial.png",
-		validator: function(credencial) {
-			function isNumber(n) {
-				return !isNaN(parseFloat(n)) && isFinite(n)
-			}
-			var split = credencial.split(" ");
-			return {
-				valid: split.length === 2 && !isNumber(split[0]) && isNumber(split[1]),
-				hint: "Aseguresé de ingresar su credencial en formato \"ABC 12345\" con un espacio."
-			};
-		}
-	}]
-  };
 
   var script_url = '//cdn.auth0.com/js/lock/10.5.0/lock.js';
 
@@ -71,6 +33,43 @@
 
       var client_id = Discourse.SiteSettings.auth0_client_id;
       var domain = Discourse.SiteSettings.auth0_domain;
+      var lock_options = {
+      	language: "es",
+	auth: {
+	  responseType: 'code',
+	  redirectUrl:  Discourse.SiteSettings.auth0_callback_url
+	},
+	theme: {
+	  logo: "https://recursos.partidodigital.org.uy/assets/img/logo_original.svg",
+	  primaryColor: "#F37021"
+	},
+	allowForgotPassword: true,
+	allowLogin: true,
+	loginAfterSignUp: false,
+	mustAcceptTerms: true,
+	languageDictionary: {
+	  signUpTerms: "Acepto los <a href='https://partidodigital.com/documentos/terminos-de-uso' target='_new'>términos de uso</a> y <a href='https://partidodigital.com/documentos/privacidad-de-datos' target='_new'>privacidad de datos</a>.",
+	  title: "Acceso",
+	  success: {
+	    signUp: "Registro completado exitosamente. Chequea tu correo para verificar tu dirección y seguir con los próximos pasos."
+  	  }
+	},
+	additionalSignUpFields: [{
+	  name: "credencial",
+	  placeholder: "ingrese su credencial",
+	  icon: "https://recursos.partidodigital.org.uy/assets/img/credencial.png",
+	  validator: function(credencial) {
+	    function isNumber(n) {
+	      return !isNaN(parseFloat(n)) && isFinite(n)
+	    }
+	    var split = credencial.split(" ");
+            return {
+              valid: split.length === 2 && !isNumber(split[0]) && isNumber(split[1]),
+	      hint: "Aseguresé de ingresar su credencial en formato \"ABC 12345\" con un espacio."
+	    };
+	  }
+        }]
+      };
 
       lock = new Auth0Lock(client_id, domain, lock_options);
 
