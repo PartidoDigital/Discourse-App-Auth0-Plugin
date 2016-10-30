4a7bd5fdf01b0d26a75a8a58650b11dbc31f90c8
Leo Giovanetti <hello@leog.me>
2016-10-30 20:12:52 -0300

    Primer intento de configuración del SSO

--- assets/javascripts/auth0.js
+++ assets/javascripts/auth0.js
@@ -1,12 +1,12 @@
 /* global Auth0Lock */
-(function () {
+(function() {
   function appendScript(src, callback) {
     var new_script = document.createElement('script');
     new_script.setAttribute('src', src);
     new_script.onload = callback;
     document.head.appendChild(new_script);
   }
-  
+
   function appendLink(href) {
     var new_link = document.createElement("link");
     new_link.setAttribute('type', 'text/css');
@@ -19,8 +19,8 @@
 
   var script_url = '//cdn.auth0.com/js/lock/10.5.0/lock.js';
 
-  appendScript(script_url, function () {
-    var checkInterval = setInterval(function () {
+  appendScript(script_url, function() {
+    var checkInterval = setInterval(function() {
       if (!Discourse.SiteSettings) {
         return;
       }
@@ -34,53 +34,91 @@
       var client_id = Discourse.SiteSettings.auth0_client_id;
       var domain = Discourse.SiteSettings.auth0_domain;
       var lock_options = {
-      	language: "es",
-	auth: {
-	  responseType: 'code',
-	  redirectUrl:  Discourse.SiteSettings.auth0_callback_url
-	},
-	theme: {
-	  logo: "https://recursos.partidodigital.org.uy/assets/img/logo_original.svg",
-	  primaryColor: "#F37021"
-	},
-	allowForgotPassword: true,
-	allowLogin: true,
-	loginAfterSignUp: false,
-	mustAcceptTerms: true,
-	languageDictionary: {
-	  signUpTerms: "Acepto los <a href='https://partidodigital.com/documentos/terminos-de-uso' target='_new'>términos de uso</a> y <a href='https://partidodigital.com/documentos/privacidad-de-datos' target='_new'>privacidad de datos</a>.",
-	  title: "Acceso",
-	  success: {
-	    signUp: "Registro completado exitosamente. Chequea tu correo para verificar tu dirección y seguir con los próximos pasos."
-  	  }
-	},
-	additionalSignUpFields: [{
-	  name: "credencial",
-	  placeholder: "ingrese su credencial",
-	  icon: "https://recursos.partidodigital.org.uy/assets/img/credencial.png",
-	  validator: function(credencial) {
-	    function isNumber(n) {
-	      return !isNaN(parseFloat(n)) && isFinite(n)
-	    }
-	    var split = credencial.split(" ");
+        language: "es",
+        auth: {
+          responseType: 'code',
+          redirectUrl: Discourse.SiteSettings.auth0_callback_url
+        },
+        theme: {
+          logo: "https://recursos.partidodigital.org.uy/assets/img/logo_original.svg",
+          primaryColor: "#F37021"
+        },
+        allowForgotPassword: true,
+        allowLogin: true,
+        loginAfterSignUp: false,
+        mustAcceptTerms: true,
+        languageDictionary: {
+          signUpTerms: "Acepto los <a href='https://partidodigital.com/documentos/terminos-de-uso' target='_new'>términos de uso</a> y <a href='https://partidodigital.com/documentos/privacidad-de-datos' target='_new'>privacidad de datos</a>.",
+          title: "Acceso",
+          success: {
+            signUp: "Registro completado exitosamente. Chequea tu correo para verificar tu dirección y seguir con los próximos pasos."
+          }
+        },
+        additionalSignUpFields: [{
+          name: "credencial",
+          placeholder: "ingrese su credencial",
+          icon: "https://recursos.partidodigital.org.uy/assets/img/credencial.png",
+          validator: function(credencial) {
+            function isNumber(n) {
+              return !isNaN(parseFloat(n)) && isFinite(n)
+            }
+            var split = credencial.split(" ");
             return {
               valid: split.length === 2 && !isNumber(split[0]) && isNumber(split[1]),
-	      hint: "Aseguresé de ingresar su credencial en formato \"ABC 12345\" con un espacio."
-	    };
-	  }
+              hint: "Aseguresé de ingresar su credencial en formato \"ABC 12345\" con un espacio."
+            };
+          }
         }]
       };
 
       lock = new Auth0Lock(client_id, domain, lock_options);
+      var auth0 = new Auth0({
+        domain: domain,
+        clientID: client_id,
+		callbackUrl: Discourse.SiteSettings.auth0_callback_url,
+        callbackOnLocationHash: true
+      });
+
+      // Handle authenticated event to store id_token in localStorage
+      lock.on("authenticated", function(authResult) {
+        isAuthCallback = true;
+
+        lock.getProfile(authResult.idToken, function(error, profile) {
+          if (error) {
+            console.error(error);
+            return;
+          }
+
+          localStorage.setItem('userToken', authResult.idToken);
+          return;
+        });
+      });
+
+      var isAuthCallback = false;
+
+      // Get the user token if we've saved it in localStorage before
+      var idToken = localStorage.getItem('userToken');
+      if (!idToken) {
+        // user is not logged, check whether there is an SSO session or not
+        auth0.getSSOData(function(err, data) {
+          if (!isAuthCallback && !err && data.sso) {
+            // there is! redirect to Auth0 for SSO
+            auth0.signin({
+              connection: data.lastUsedConnection.name,
+              scope: 'openid name picture'
+            });
+          }
+        });
+      }
 
     }, 300);
   });
-	
+
   appendLink('https://recursos.partidodigital.org.uy/assets/css/auth0.css');
 
   var LoginController = require('discourse/controllers/login').default;
   LoginController.reopen({
-    authenticationComplete: function () {
+    authenticationComplete: function() {
       if (lock) {
         lock.hide();
       }
@@ -100,7 +138,7 @@
 
         this.controllerFor('login').resetForm();
       },
-      showCreateAccount: function () {
+      showCreateAccount: function() {
         if (!Discourse.SiteSettings.auth0_client_id || Discourse.SiteSettings.auth0_connection !== '') {
           return this._super();
         }
